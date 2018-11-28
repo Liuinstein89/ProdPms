44adaa1c0c36d28f86d61c4b653f4557f9cfa1dc
workLapTop <zwkssdfz@163.com>
2018-11-28 18:14:24 +0800

    2

--- ProdPms/src/main/resources/static/js/common.js
+++ ProdPms/src/main/resources/static/js/common.js
@@ -1,168 +1,85 @@
-layui.config({
-	base: '../static/js/module/'
-}).extend({
-	dialog: 'dialog',
+
+layui.use(['form', 'jquery', 'laydate', 'element'], function() {
+	var form = layui.form;
+	var	$ = layui.jquery;
+
+	//获取cookies中的用户名 数据
+
+    window.showName = function(id,name) {
+        if(getCookie(name)!=null){
+            $("#"+id).val(getCookie(name));
+        }
+    }
+
+    // alert(getCookie("name"));
+    window.getCookie = function(name) {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)"); //正则匹配
+        if(arr=document.cookie.match(reg)){
+            return unescape(arr[2]);
+        }
+        else{
+            return null;
+        }
+    }
+
+    window.loadSelect = function(form,url,id,selval,selname,value){
+        $.ajax({
+            url : url,
+            type : "GET",
+            async : false,
+            data:{page:1,limit:10000},
+            dataType:"json",
+            async: true,//异步执行
+            success : function(res) {
+                // layer.alert(JSON.stringify(res.data.list));
+                $("#"+id).html("");
+                $("#"+id).append('<option selected="" value="">请选择</option>');
+
+                data1 = (res.data.list);
+
+                // layer.alert(data1[0][selval]);
+
+                // /*<![CDATA[*/
+                // for(var i=0;i<res.data.total;i++){
+                //     if(data1[i].id==value){
+                //         $("#"+id).append('<option selected="" value='+data1[i].id+'>'+data1[i].funcName+'</option>');
+                //     }else{
+                //         $("#"+id).append('<option value='+data1[i].id+'>'+data1[i].funcName+'</option>');
+                //     }
+                // }
+                // /*]]>*/
+
+                /*<![CDATA[*/
+                for(var i=0;i<res.data.total;i++){
+                    if(data1[i][selval]==value){
+                        $("#"+id).append('<option selected="" value='+data1[i][selval]+'>'+data1[i][selname]+'</option>');
+                    }else{
+                        $("#"+id).append('<option value='+data1[i][selval]+'>'+data1[i][selname]+'</option>');
+                    }
+                }
+                /*]]>*/
+                form.render();//重新渲染
+            }
+        });
+    }
+
+
+    //解析从url传过来的参数
+    /*<![CDATA[*/
+    window.getRequestParam = function(){
+        var urlInfo = window.location.href;
+        var argsIndex = urlInfo.indexOf("?");
+        var args = urlInfo.substring((argsIndex + 1)).split("&");
+        var argsInfo = "{";
+        for ( var i = 0; i < args.length; i++) {
+            var argResult = args[i].split("=");
+            argsInfo = argsInfo + "'" + argResult[0] + "':'" + argResult[1] + "'";
+            if (i != args.length - 1) {
+                argsInfo += ',';
+            }
+        }
+        argsInfo += "}";
+        return eval('(' + argsInfo + ')');
+    }
+    /*]]>*/
 });
-
-layui.use(['form', 'jquery', 'laydate', 'layer', 'laypage', 'dialog',   'element'], function() {
-	var form = layui.form(),
-		layer = layui.layer,
-		$ = layui.jquery,
-		dialog = layui.dialog;
-	//获取当前iframe的name值
-	var iframeObj = $(window.frameElement).attr('name');
-	//全选
-	form.on('checkbox(allChoose)', function(data) {
-		var child = $(data.elem).parents('table').find('tbody input[type="checkbox"]');
-		child.each(function(index, item) {
-			item.checked = data.elem.checked;
-		});
-		form.render('checkbox');
-	});
-	//渲染表单
-	form.render();	
-	//顶部添加
-	$('.addBtn').click(function() {
-		var url=$(this).attr('data-url');
-		//将iframeObj传递给父级窗口,执行操作完成刷新
-		parent.page("菜单添加", url, iframeObj, w = "700px", h = "620px");
-		return false;
-
-	}).mouseenter(function() {
-
-		dialog.tips('添加', '.addBtn');
-
-	})
-	//顶部排序
-	$('.listOrderBtn').click(function() {
-		var url=$(this).attr('data-url');
-		dialog.confirm({
-			message:'您确定要进行排序吗？',
-			success:function(){
-				layer.msg('确定了')
-			},
-			cancel:function(){
-				layer.msg('取消了')
-			}
-		})
-		return false;
-
-	}).mouseenter(function() {
-
-		dialog.tips('批量排序', '.listOrderBtn');
-
-	})	
-	//顶部批量删除
-	$('.delBtn').click(function() {
-		var url=$(this).attr('data-url');
-		dialog.confirm({
-			message:'您确定要删除选中项',
-			success:function(){
-				layer.msg('删除了')
-			},
-			cancel:function(){
-				layer.msg('取消了')
-			}
-		})
-		return false;
-
-	}).mouseenter(function() {
-
-		dialog.tips('批量删除', '.delBtn');
-
-	})	
-	//列表添加
-	$('#table-list').on('click', '.add-btn', function() {
-		var url=$(this).attr('data-url');
-		//将iframeObj传递给父级窗口
-		parent.page("菜单添加", url, iframeObj, w = "700px", h = "620px");
-		return false;
-	})
-	//列表删除
-	$('#table-list').on('click', '.del-btn', function() {
-		var url=$(this).attr('data-url');
-		var id = $(this).attr('data-id');
-		dialog.confirm({
-			message:'您确定要进行删除吗？',
-			success:function(){
-				layer.msg('确定了')
-			},
-			cancel:function(){
-				layer.msg('取消了')
-			}
-		})
-		return false;
-	})
-	//列表跳转
-	$('#table-list,.tool-btn').on('click', '.go-btn', function() {
-		var url=$(this).attr('data-url');
-		var id = $(this).attr('data-id');
-		window.location.href=url+"?id="+id;
-		return false;
-	})
-	//编辑栏目
-	$('#table-list').on('click', '.edit-btn', function() {
-		var That = $(this);
-		var id = That.attr('data-id');
-		var url=That.attr('data-url');
-		//将iframeObj传递给父级窗口
-		parent.page("菜单编辑", url + "?id=" + id, iframeObj, w = "700px", h = "620px");
-		return false;
-	})
-});
-
-/**
- * 控制iframe窗口的刷新操作
- */
-var iframeObjName;
-
-//父级弹出页面
-function page(title, url, obj, w, h) {
-	if(title == null || title == '') {
-		title = false;
-	};
-	if(url == null || url == '') {
-		url = "404.html";
-	};
-	if(w == null || w == '') {
-		w = '700px';
-	};
-	if(h == null || h == '') {
-		h = '350px';
-	};
-	iframeObjName = obj;
-	//如果手机端，全屏显示
-	if(window.innerWidth <= 768) {
-		var index = layer.open({
-			type: 2,
-			title: title,
-			area: [320, h],
-			fixed: false, //不固定
-			content: url
-		});
-		layer.full(index);
-	} else {
-		var index = layer.open({
-			type: 2,
-			title: title,
-			area: [w, h],
-			fixed: false, //不固定
-			content: url
-		});
-	}
-}
-
-/**
- * 刷新子页,关闭弹窗
- */
-function refresh() {
-	//根据传递的name值，获取子iframe窗口，执行刷新
-	if(window.frames[iframeObjName]) {
-		window.frames[iframeObjName].location.reload();
-
-	} else {
-		window.location.reload();
-	}
-
-	layer.closeAll();
-}
