b83a25df0e867ba0679d7d8893ca36e9ceab50db
workLapTop <zwkssdfz@163.com>
2019-01-14 09:37:18 +0800

    123

--- ProdPms/src/main/java/com/ccb/ProdPms/ProdPmsApplication.java
+++ ProdPms/src/main/java/com/ccb/ProdPms/ProdPmsApplication.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
@@ -22,16 +21,6 @@ public class ProdPmsApplication extends SpringBootServletInitializer {
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ProdPmsApplication.class);
     }*/
-
-
-	/*
-	 * @Bean public MultipartConfigElement multipartConfigElement() {
-	 * MultipartConfigFactory factory = new MultipartConfigFactory(); //// 设置文件大小限制
-	 * ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了; factory.setMaxFileSize("128KB"); //KB,MB ///
-	 * 设置总上传数据总大小 factory.setMaxRequestSize("256KB"); //Sets the directory location
-	 * wherefiles will be stored. //factory.setLocation("路径地址"); return
-	 * factory.createMultipartConfig(); }
-	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProdPmsApplication.class, args);
